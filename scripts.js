972fb9fa2f33df0fe0f68e901e325fd30ff6fe44
Gerard Martinez <82490038+forming3d@users.noreply.github.com>
2025-03-14 11:37:27 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,18 @@
-// Ejemplo de interacción básica
-document.addEventListener('DOMContentLoaded', function() {
-    const galleryItems = document.querySelectorAll('.gallery-item');
-    
-    galleryItems.forEach(item => {
-        item.addEventListener('click', () => {
-            alert('Ver detalles del proyecto');
+document.addEventListener('DOMContentLoaded', function () {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navMenu = document.querySelector('.nav-menu');
+
+    menuToggle.addEventListener('click', function () {
+        navMenu.classList.toggle('open');
+        menuToggle.classList.toggle('open');
+    });
+
+    // Cerrar el menú al hacer clic en un enlace
+    const navLinks = document.querySelectorAll('.nav-menu a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            navMenu.classList.remove('open');
+            menuToggle.classList.remove('open');
         });
     });
 });
