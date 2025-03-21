c59f875038063e1cce46d9234cda9dd342538f0b
Gerard Martinez <82490038+forming3d@users.noreply.github.com>
2025-03-21 17:50:50 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navMenu = document.querySelector('.nav-menu');
+
+    menuToggle.addEventListener('click', function () {
+        navMenu.classList.toggle('open');
+        menuToggle.classList.toggle('open');
+    });
+
+    // Cerrar el menú al hacer clic en un enlace
+    const navLinks = document.querySelectorAll('.nav-menu a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            navMenu.classList.remove('open');
+            menuToggle.classList.remove('open');
+        });
+    });
+});
